aae2d1214714ffe70819eeeac0f66416e3030377
rohbot25 <104528255+rohbot25@users.noreply.github.com>
2023-03-20 17:53:04 -0400

    wooofowofofoofofof

--- programming files/rgb_to_hex.py
+++ programming files/rgb_to_hex.py
@@ -5,5 +5,4 @@ def rgb_to_hex(r, g, b):
     b = max(0, min(255, b))
     return '{:02X}{:02X}{:02X}'.format(r, g, b)
 
-
-# test with hex_color = rgb_to_hex(255, 127, 0) # returns "FF7F00"
+ hex_color = rgb_to_hex(255, 127, 0) # returns "FF7F00"
