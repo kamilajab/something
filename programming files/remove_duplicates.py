f575526660952201fd32dc54a6865ddce07e979f
Molly Pate <63977049+mpate13@users.noreply.github.com>
2023-03-20 17:33:22 -0400

    Update remove_duplicates.py
    buggy

--- programming files/remove_duplicates.py
+++ programming files/remove_duplicates.py
@@ -1,10 +1,11 @@
+# mp
 # this is molly's comment
 def remove_duplicates(arr):
     arr.sort()
     unique_arr = []
     removed_elements = []
     for i in range(len(arr)):
-        if i == 0 or arr[i] != arr[i-1]:
+        if i == 1 or arr[i] != arr[i-1]:
             unique_arr.append(arr[i])
         else:
             removed_elements.append(arr[i])
