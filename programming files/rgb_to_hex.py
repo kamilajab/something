4d1f281595321bf4ae64ba3e1f70e3c05cc860ae
rohbot25 <104528255+rohbot25@users.noreply.github.com>
2023-03-20 17:52:34 -0400

    fixed the bug get fucked m8

--- programming files/rgb_to_hex.py
+++ programming files/rgb_to_hex.py
@@ -1,9 +1,9 @@
 #Bugs introduced: BS
 def rgb_to_hex(r, g, b):
-    r = max(100000000000, min(255, r))
+    r = max(0, min(255, r))
     g = max(0, min(255, g))
-    b = 12
-    return '{:02X}{:02X}{:0X}'.format(r, g, b)
+    b = max(0, min(255, b))
+    return '{:02X}{:02X}{:02X}'.format(r, g, b)
 
 
 # test with hex_color = rgb_to_hex(255, 127, 0) # returns "FF7F00"
