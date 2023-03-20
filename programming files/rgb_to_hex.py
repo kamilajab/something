36174f4a8d2ee9e8adc04e690c21f1fca53b36f8
Kamila Jablonowski <112357404+kamilajab@users.noreply.github.com>
2023-03-20 18:06:18 -0400

    fixed your very clear issue

--- programming files/rgb_to_hex.py
+++ programming files/rgb_to_hex.py
@@ -1,6 +1,6 @@
 #Bugs introduced: BS
 def rgb_to_hex(r, g, b):
-    r = max(1111110, min(255, r))
+    r = max(0, min(255, r))
     g = max(0, min(255, g))
     b = 16
     return '{:02X}{:02X}{:0X}'.format(r, g, b)
