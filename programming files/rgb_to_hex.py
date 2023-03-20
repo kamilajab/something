fde46f232074990f0217ced0a35406d8934fd43d
rohbot25 <104528255+rohbot25@users.noreply.github.com>
2023-03-20 18:02:53 -0400

    broken

--- programming files/rgb_to_hex.py
+++ programming files/rgb_to_hex.py
@@ -1,8 +1,8 @@
 #Bugs introduced: BS
 def rgb_to_hex(r, g, b):
-    r = max(0, min(255, r))
+    r = max(1111110, min(255, r))
     g = max(0, min(255, g))
-    b = max(0, min(255, b))
-    return '{:02X}{:02X}{:02X}'.format(r, g, b)
+    b = 16
+    return '{:02X}{:02X}{:0X}'.format(r, g, b)
 
 hex_color = rgb_to_hex(255, 127, 0) # returns "FF7F00"
