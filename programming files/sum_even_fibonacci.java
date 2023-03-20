88803d5d821a1955fc3c525bbb00a0af05551530
rohbot25 <104528255+rohbot25@users.noreply.github.com>
2023-03-20 17:34:55 -0400

    Update sum_even_fibonacci.java
    really sad

--- programming files/sum_even_fibonacci.java
+++ programming files/sum_even_fibonacci.java
@@ -1,16 +1,17 @@
+//er
 public class sum_even_fibonacci {
     public static void main(String[] args) {
-        int a = 0, b = 1;
-        int sumEven = 0;
+        int a = 0, b = 1
+        int sumEven = 0
         while (b < 4000000) {
             if (b % 2 == 0) {
-                sumEven += b;
+                sumEven += b
             }
-            int temp = b;
-            b = a + b;
-            a = temp;
+            temp = b
+            b = a + b
+            a += temp
         }
-        System.out.println(sumEven);
+        System.out.prinln(sumEven)
     }
 }
 
