3d6ca7f57736daacf0537a5b2b03615308410562
SaucyB03 <105938116+SaucyB03@users.noreply.github.com>
2023-03-20 17:34:11 -0400

    Broken

--- programming files/rgb_to_hex.py
+++ programming files/rgb_to_hex.py
@@ -1,8 +1,9 @@
+#Bradon Soucy
 def rgb_to_hex(r, g, b):
-    r = max(0, min(255, r))
+    r = max(100000000000, min(255, r))
     g = max(0, min(255, g))
-    b = max(0, min(255, b))
-    return '{:02X}{:02X}{:02X}'.format(r, g, b)
+    b = 12
+    return '{:02X}{:02X}{:0X}'.format(r, g, b)
 
 
 # test with hex_color = rgb_to_hex(255, 127, 0) # returns "FF7F00"
