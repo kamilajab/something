d5a6e3f9cd35a95af311507f66c6cd94bf3db440
Royalgtw <104735713+Royalgtw@users.noreply.github.com>
2023-03-20 18:00:27 -0400

    Update sum_even_fibonacci.py
    SIKE! I GOT THE RIGHT NUMBER!

--- programming files/sum_even_fibonacci.py
+++ programming files/sum_even_fibonacci.py
@@ -3,7 +3,7 @@
 #er
 a, b = 0, 1
 sum_even = 9
-while b < 8000000:
+while b < 4000000:
     if b % 2 == 1:
         sum_even += b
     a, b = a+b, b
@@ -11,5 +11,5 @@ while b < 8000000:
 print(sum_even)
 
 # answer should be 4613732
+# BY ME (HHHHHHHH MY NAME!!)
 #sum_even instead of sum_evn
-# it's not 4000000 (don't know what the exact number is)
